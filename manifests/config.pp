aee4fc2bd02eadb9fb75983975a647868b673764
Eder Roger Souza <eder.roger.souza@gmail.com>
2015-02-20 19:47:14 -0200

    remove AngularMongo

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,12 +17,6 @@ package { 'git':
   ensure => present
 }
 
-#exec { 'angular_mongo':
-#  command => "git clone https://github.com/EderRoger/AngularMongo.git",
-#  path => ["/bin", "/usr/bin"], 
-#  require => Package['git'],
-#}
-
 package { nodejs: 
    require => Exec["add_node_package"]
 }
@@ -46,23 +40,4 @@ exec { "install_bower":
    require => Package["nodejs"]
 }
 
-exec { 'start_app':
-  command => "npm start",
-  path => ["/bin", "/usr/bin"],
-  cwd => '/vagrant/AngularMongo',
-  require => Exec["install_bower"]
-}
-
-#exec { "npm_install":
-# command => "cd /vagrant/AngularMongo & npm install",
-#  user => 'root',
-#  path    => "/usr/local/bin/:/bin/",
-#  require => Exec["add_node_package"]
-#}
-
-#exec { "my_exec_task":
-#  command => "tar zxf /home/user/tmp/test.tar.gz",
-#  creates => "/home/user/tmp/new_directory",
-#  path    => "/usr/local/bin/:/bin/",
-#}
 
